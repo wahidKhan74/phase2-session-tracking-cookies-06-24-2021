e7361e28f1414f6f073972e0ae0ce3048ad1160e
Waheed Khan <wahid.khan74@gmail.com>
2021-06-24 05:43:11 +0000

    adding logout

---
+++ src/com/dell/webapp/Logout.java
@@ -0,0 +1,44 @@
+package com.dell.webapp;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Logout
+ */
+@WebServlet("/logout")
+public class Logout extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+ 
+    public Logout() {}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();		
+		// nav bar
+		request.getRequestDispatcher("index.html").include(request, response);
+		// logout
+		Cookie ck = new Cookie("xemail", "");
+		ck.setMaxAge(0);
+		response.addCookie(ck);
+		out.println("<h3 style='color:green'>sucessfull logout </h3>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
